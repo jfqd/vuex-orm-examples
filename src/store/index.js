bb3bee2aab3dc46fc34e660ffa95cd78385efe9e
jfqd <jfqd@blun.org>
2019-08-27 08:52:52 +0200

    include config for vuex-orm-localforage

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import VuexORM from '@vuex-orm/core'
 import database from '@/database'
+import VuexORMLocalForage from 'vuex-orm-localforage'
 
 Vue.use(Vuex)
 
+VuexORM.use(VuexORMLocalForage, {
+  database
+})
+
 const store = new Vuex.Store({
+  namespaced: true,
   plugins: [VuexORM.install(database)]
 })
 
